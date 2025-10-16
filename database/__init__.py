b01c5f303db9394a36d5b5236782ac5d16134a53
Isabela Santos <isabela@dellapiazza.com>
2025-10-16 16:25:19 +0100

    syntax problem

--- database/__init__.py
+++ database/__init__.py
@@ -2,25 +2,5 @@
 Database operations for HeritageAI
 """
 
-from .database import (
-    get_db_connection,
-    create_tables,
-    insert_record,
-    search_records,
-    get_all_collections,
-    create_collection,
-    get_collection_items
-)
-
-from .processing_operations import ProcessingDatabase
-
-__all__ = [
-    'get_db_connection',
-    'create_tables',
-    'insert_record',
-    'search_records',
-    'get_all_collections',
-    'create_collection',
-    'get_collection_items',
-    'ProcessingDatabase'
-]
+# This file makes database/ a package
+# Import modules directly like: from database.processing_operations import ProcessingDatabase
